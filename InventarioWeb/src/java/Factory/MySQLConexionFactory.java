2195807c6dd565cdfd704ff5c6e1ae52cd3b6bd9
Adonis <adonisbautista39@gmail.com>
2020-06-11 03:14:28 -0600

    Agregamos la palabra final para que no se pueda extender la clase en otras, además la agregar extends ConexionDB lo cual permitirá utilizar los métodos abstractos de esa clase y tambien implementamos los métodos abstractos .

--- InventarioWeb/src/java/Factory/MySQLConexionFactory.java
+++ InventarioWeb/src/java/Factory/MySQLConexionFactory.java
@@ -1,5 +1,12 @@
 package Factory;
 
-public class MySQLConexionFactory {
+import java.sql.Connection;
+
+public final class MySQLConexionFactory extends ConexionBD {
+
+    @Override
+    Connection open() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
