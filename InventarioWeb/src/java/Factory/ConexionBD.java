0e2b4886687ea5ed35047c96d766e94a03260b2b
Adonis <adonisbautista39@gmail.com>
2020-06-11 02:56:02 -0600

    Hemos Creado el método que se encargara de las consultas y su respectivo codigo del método

--- InventarioWeb/src/java/Factory/ConexionBD.java
+++ InventarioWeb/src/java/Factory/ConexionBD.java
@@ -1,6 +1,10 @@
 package Factory;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public abstract class ConexionBD {
   protected String[] parametros; //Array que recibe los parametros de conexión
@@ -8,4 +12,18 @@ public abstract class ConexionBD {
   //El siguiente método abstracto no se implementa solamente se declara , se 
   //implementa en la subclase
   abstract Connection open(); // Método abstracto que devulve un objeto connection
+
+
+//Crear método para las consultas
+public ResultSet cosultaSQL(String consulta){
+Statement st; //Objeto Statement en el encargado de ejecutar las consultas
+ResultSet rs = null; //Tabla temporal donde se almacenan los datos
+try{
+  st = conexion.createStatement();
+  rs = st.executeQuery(consulta); // Se ejecuta la consulta
+}catch(SQLException ex){
+      ex.printStackTrace();
+}
+return rs;
+}
 }
