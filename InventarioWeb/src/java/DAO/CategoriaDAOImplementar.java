e6425c216666e11a1502750667bc45f89c49be8c
Adonis <adonisbautista39@gmail.com>
2020-06-11 04:26:37 -0600

    He creado la clase llamada  CategoriaDAOImplementar dentro del paquete DAO , esta clase nos servira para la implementación de los métodos , luego de crearla implementamos el metodo abstracto de la clase CategoriaDAO

---
+++ InventarioWeb/src/java/DAO/CategoriaDAOImplementar.java
@@ -0,0 +1,33 @@
+package DAO;
+
+import Model.Categoria;
+import java.util.List;
+
+public class CategoriaDAOImplementar implements CategoriaDAO {
+
+    @Override
+    public List<Categoria> Listar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Categoria> Listar2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Categoria editarCat(int id_cat_edit) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean guardarCat(Categoria categoria) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean borrarCat(int id_cat_borrar) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
