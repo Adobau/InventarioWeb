360541ffecbdee7a5c872eae2c3a563fe39fe496
Adonis <adonisbautista39@gmail.com>
2020-06-11 02:42:07 -0600

    Hemos agregado el metodo abstracto en la clase ConexionBD

--- InventarioWeb/src/java/Factory/ConexionBD.java
+++ InventarioWeb/src/java/Factory/ConexionBD.java
@@ -1,6 +1,11 @@
 package Factory;
 
+import java.sql.Connection;
 
-public class ConexionBD {
-    
+public abstract class ConexionBD {
+  protected String[] parametros; //Array que recibe los parametros de conexión
+  protected Connection conexion;
+  //El siguiente método abstracto no se implementa solamente se declara , se 
+  //implementa en la subclase
+  abstract Connection open(); // Método abstracto que devulve un objeto connection
 }
